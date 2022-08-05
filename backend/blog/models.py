43157e59b1dad812d3d3a43a4c42a426180b1525
Matias Cardenas <cardenasmatias.1990@gmail.com>
2022-08-05 21:48:59 +0200

    feat: adding models, migrations and black & isort as formatting linting tools

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,42 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+    )
+    website = models.URLField(blank=True)
+    bio = models.CharField(max_length=240, blank=True)
+
+    def __str__(self):
+        return self.user.get_username()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    class Meta:
+        ordering = ["-publish_date"]
+
+    title = models.CharField(max_length=255, unique=True)
+    subtitle = models.CharField(max_length=255, blank=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    body = models.TextField()
+    meta_description = models.CharField(max_length=150, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    publish_date = models.DateTimeField(blank=True, null=True)
+    published = models.BooleanField(default=False)
+
+    # on_delete=models.PROTECT argument for author ensures that you won’t accidentally delete an author who still has posts on the blog
+    author = models.ForeignKey(Profile, on_delete=models.PROTECT)
+
+    # The ManyToManyField relationship to Tag allows you to associate a post with zero or more tags. Each tag can be associated to many posts.
+    tags = models.ManyToManyField(Tag, blank=True)
